ea861bc64c9c89c68e2b7503a36bef21225daa4f
Sarunyamk <Sarunya46mk@gmail.com>
2024-11-05 10:41:49 +0700

    edit /multer

--- middlewares/upload-multer.js
+++ middlewares/upload-multer.js
@@ -4,9 +4,9 @@ const multer = require('multer')
 const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, path.join(__dirname, '../upload_pic')),
     filename: (req, file, cb) => {
-        const userId = req.user ? req.user.id : 'guest';  // ใช้ 'guest' เมื่อไม่มี req.user
-        // const {id} = req.user
-        const fullFilename = `${userId}_${Date.now()}_${path.extname(file.originalname)}`
+
+        const { id } = req.user
+        const fullFilename = `${id}_${Date.now()}_${path.extname(file.originalname)}`
         cb(null, fullFilename)
     }
 })
